40784f9e2530771c55af8b640371ea9847f9bbba
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2024-03-22 18:52:21 +0300

    add config file

---
+++ conf.py
@@ -0,0 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+USERS_PROD_PORT = os.environ.get("USERS_PROD_PORT")
+USERS_TEST_PORT = os.environ.get("USERS_TEST_PORT")
+FREE_PROVIDER_PORT = os.environ.get("FREE_PROVIDER_PORT")
+PAID_PROVEDER_PORT = os.environ.get("PAID_PROVEDER_PORT")
